d5bc109ae5c6982baa11826885a4374117ac40a1
Méhix <77546563+MEHIX7@users.noreply.github.com>
2023-04-20 20:40:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function openMenuMobile() {
+    document.querySelector('.header__nav').classList.add('open')
+    document.querySelector('.overlay__menu__mobile').classList.add('open')
+}
+
+function CloseMenuMobile() {
+    document.querySelector('.header__nav').classList.remove('open')
+    document.querySelector('.overlay__menu__mobile').classList.remove('open')
+}
